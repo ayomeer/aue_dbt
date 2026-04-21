d82f86184ce87c9089fcd616c587162f5f1abdbc
Andreas <ayomeer@gmail.com>
2026-04-21 14:05:55 +0200

    generalized sources to match selected target (fix)

--- src/dbt/dbt_dbu_aue_quellkataster/macros/generate_schema_name.sql
+++ src/dbt/dbt_dbu_aue_quellkataster/macros/generate_schema_name.sql
@@ -1,12 +1,11 @@
+-- Overrides standard schema naming behavior:
+-- Arguments:
+-- custom_schema_name: schema name explicitly defined dusing 
+--                     schema model configuration
+-- node: dbt model metadata
 
+-- default schema is the one defined in profiles.yml
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    -- Overrides standard schema naming behavior:
-    -- Argumens:
-    -- custom_schema_name: schema name explicitly defined dusing 
-    --                     schema model configuration
-    -- node: dbt model metadata
-
-    -- default schema is the one defined in profiles.yml
     {%- set default_schema = target.schema -%}
 
     {%- if custom_schema_name is none -%}
