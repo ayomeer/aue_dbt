af39642aff056403000a8ad23e376863e87995bb
Andreas <ayomeer@gmail.com>
2026-04-24 08:07:52 +0000

    refactor macro layout

--- src/dbt/dbt_dbu_aue_quellkataster/macros/interlis_utils/ili_utils.sql
+++ src/dbt/dbt_dbu_aue_quellkataster/macros/interlis_utils/ili_utils.sql
@@ -1,14 +1,29 @@
--- Managing INTERLIS Boundary ---------------------------------------
+--- General Utility -----------------------------------------------------------
 
-{% macro run_start_parsing() %}
-  -- reset dbt schema's t_ili2db_seq
-  {{ reset_ili_sequence(target.schema) }}
-  
-  {% if var('reset_target', false) %}
-    {{ reset_target_schema(var('target_ili_schema')) }}
-  {% endif %}
+-- Create t_ili2db_sequence
+-- intended use: first time dbt schema setup
+--  call through run-operations e.g.:
+--  dbt run-operation create_ili_sequence --args '{schema: dbt_quellkataster}'
+{% macro create_ili_sequence(schema) -%}
+  -- create sequence
+  {% set query %}
+    CREATE SEQUENCE IF NOT EXISTS {{schema}}.t_ili2db_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+  {% endset %}
+  {% set query_return = run_query(query) %}
+
 {%- endmacro %}
 
+-- Reset 't_ili2db_seq' in target schema
+{% macro reset_ili_sequence(schema) -%}
+  ALTER SEQUENCE {{schema}}.t_ili2db_seq RESTART WITH 1;
+{%- endmacro %}
+
+--- Baskets and Datasets ------------------------------------------------------
 
 {% macro setup_baskets(schema_name) %}
   -- Populate basket table based on project configuration:
@@ -85,8 +100,7 @@
 {%- endmacro %}
 
 
-
-
+--- Transferring across boundary: dbt -> INTERLIS schema ----------------------
 
 -- Export dbt table to target table
 {% macro transfer_table(schema_name, table_name) %}
@@ -106,31 +120,13 @@
 {%- endmacro %}
 
 
--- Managing INTERLIS concepts on dbt side -------------------------------------
-
--- Create t_ili2db_sequence
--- intended use: first time dbt schema setup
---  call through run-operations e.g.:
---  dbt run-operation create_ili_sequence --args '{schema: dbt_quellkataster}'
-{% macro create_ili_sequence(schema) -%}
-  -- create sequence
-  {% set query %}
-    CREATE SEQUENCE IF NOT EXISTS {{schema}}.t_ili2db_seq
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
-  {% endset %}
-  {% set query_return = run_query(query) %}
-
+--- Parsing on dbt Triggers ---------------------------------------------------
+{% macro run_start_parsing() %}
+  -- reset dbt schema's t_ili2db_seq
+  {{ reset_ili_sequence(target.schema) }}
+  
+  {% if var('reset_target', false) %}
+    {{ reset_target_schema(var('target_ili_schema')) }}
+  {% endif %}
 {%- endmacro %}
-
--- Reset 't_ili2db_seq' in target schema
-{% macro reset_ili_sequence(ili_schema) -%}
-
-ALTER SEQUENCE {{ili_schema}}.t_ili2db_seq RESTART WITH 1;
-
-{%- endmacro %}
-
 
