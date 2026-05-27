84646f6bf016346c6239a418ee49514c2fb38e24
Andreas <ayomeer@gmail.com>
2026-05-27 13:31:34 +0000

    refactoring adding mismatch cols to rows audit as a audit_utils macros

--- src/dbt/dbt_arten/macros/audit_utils.sql
+++ src/dbt/dbt_arten/macros/audit_utils.sql
@@ -1,5 +1,10 @@
+--- Utils for generating additional info on mismatched cols for row based audit ---
 
 
+{# 
+  Takes list of column names and returns list of same column names wrapped in 'array_agg()' 
+  e.g. 'col_name' -> 'array_agg(col_name) as col_name' 
+#}
 {% macro aggregate_cols(col_list) %}
   
   {% set return_cols = [] %}
@@ -15,6 +20,11 @@
 {%- endmacro %}
 
 
+{# 
+  Takes list of of column names (assumed to be array-like) and returns list of 
+  expressions to detect mismatch between 1st and 2nd array element
+  e.g. 'col_name' -> 'col_name[1] is distinct from col_name[2] as mismatch_col_name' 
+#}
 {% macro mismatched_cols(col_list) %}
 
   {% set return_cols = [] %}
@@ -29,7 +39,40 @@
   )}}
 {%- endmacro %}
 
+{# 
 
+#}
+{% macro audit_rows_with_col_mismatched(cols_to_compare, ref_audit_rows) %}
+WITH agg_cte as (
+  SELECT 
+    dbt_audit_surrogate_key,
+    {{ aggregate_cols(cols_to_compare) }}
+
+  FROM {{ ref_audit_rows }}
+  WHERE dbt_audit_row_status = 'modified'
+  GROUP BY dbt_audit_surrogate_key
+),
+mismatched_cols_cte as (
+  SELECT
+    dbt_audit_surrogate_key as dbt_audit_surrogate_fkey,
+    {{ mismatched_cols(cols_to_compare) }}
+  FROM agg_cte
+)
+
+SELECT 
+  row_number() over() as fid,
+  CASE 
+    WHEN dbt_audit_in_a THEN 'old row'
+    WHEN dbt_audit_in_b THEN 'new row'
+    ELSE 'error'
+  END as row_shown,
+  *
+FROM {{ ref_audit_rows }} as a
+LEFT JOIN mismatched_cols_cte as m
+  ON m.dbt_audit_surrogate_fkey = a.dbt_audit_surrogate_key
+{%- endmacro %}
+
+--- Other miscellaneous utility macros
 {% macro get_datetime_string() -%}
   {{ run_started_at.astimezone(modules.pytz.timezone("Europe/Zurich")).strftime('%Y-%m-%d %H:%M:%S') }}
 {%- endmacro%}
---
+++ src/dbt/dbt_arten/models/audits/wisgl_export/audit_wisgl_export_rows_with_col_mismatches.sql
@@ -0,0 +1,28 @@
+
+{% set cols_to_compare = [
+'id_art',
+'name_d',
+'name_lat',
+'organismengruppe',
+'schutz_ch',
+'schutz_gl',
+'roteliste',
+'radius',
+'genau',
+'substrat',
+'foerdermassnahmen',
+'finder',
+'funddatum',
+'hinzugefuegt_am',
+'kantonsintern',
+'verwaltungsintern',
+'astatus',
+'bemerkungen',
+'fotos'
+]%}
+
+{{ audit_rows_with_col_mismatched(
+	cols_to_compare, 
+	ref('audit_wisgl_export_rows')
+) }}
+
