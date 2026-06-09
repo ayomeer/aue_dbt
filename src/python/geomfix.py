2cbce05152879060a59532a900e3dcec064d3235
Andreas <ayomeer@gmail.com>
2026-06-09 11:22:31 +0000

    geomfix curvature filter

--- src/python/geomfix.py
+++ src/python/geomfix.py
@@ -12,36 +12,67 @@ import matplotlib.pyplot as plt
 path_test_data = Path("/project/src/python/example_data/multipolygon.csv")
 pdf_multipolygon = pd.read_csv(path_test_data)
 
-# -- Data Setup
-
+# -- Data Setup ----------------------------------------------------------
 
 # poly selection
 pdf_poly = pdf_multipolygon[pdf_multipolygon["polygon"] == 1]
 
-x_values = np.array(pdf_poly["x"])
-y_values = np.array(pdf_poly["y"])
+x_values = np.array(pdf_poly["x"])#[:-1]
+y_values = np.array(pdf_poly["y"])#[:-1]
 
 points = np.stack((x_values, y_values), axis=1)
 
-# -- Filtering ...--------------------------------------------------------
+# -- Filtering -----------------------------------------------------------
+
+# computing discrete 2nd order derivative 
 filterKernel = [1, -2, 1]
 
+points_lead = np.roll(points, -1, axis=0)
+points_lag  = np.roll(points, 1, axis=0)
 
-
-# 2nd derivative =
 d2_vect = (
-  np.roll(points, 1) * filterKernel[0]
-  + points * filterKernel[1]
-  + np.roll(points, -1) * filterKernel[2]
+    filterKernel[0] * points_lead
+  + filterKernel[1] * points
+  + filterKernel[2] * points_lag
 )
-
 
 d2_norm = np.linalg.norm(d2_vect, axis=1)
 
+poly_lines = points_lead - points
+poly_lines_norm = np.linalg.norm(poly_lines, axis=1)
+local_line_length = poly_lines_norm + np.roll(poly_lines_norm, 1)
+
+d2_norm_scaled = d2_norm / local_line_length**3
+
 # -- Plotting ------------------------------------------------------------
 
+quiver_scaling = 5
+
+# Plot Polygon
+fig = plt.figure()
+ax = fig.add_subplot(111)
+p = plt.plot(x_values, y_values, 'o-', markersize=2)
+q = plt.quiver(
+  x_values, y_values,
+  d2_vect[:,0], d2_vect[:,1],
+  angles='xy',
+  scale_units='xy',
+  scale=quiver_scaling
+)
+
+for i, (x, y) in enumerate(points):
+  ax.annotate(
+    "idx: " + str(i) + " " + str(np.round(local_line_length[i],2)), 
+    (x, y),
+    xytext=(3,3),
+    textcoords="offset points",
+    fontsize=8
+  )
+
+
+# curvature magnitude scaled down by local line lengths plot
 plt.figure()
-plt.plot(d2_norm)
+plt.plot(d2_norm_scaled)
 plt.show()
 
 
