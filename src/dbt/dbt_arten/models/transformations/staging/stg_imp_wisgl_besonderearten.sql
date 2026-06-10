bd0affdefda2730086cc31abfb9203045791705a
Andreas <ayomeer@gmail.com>
2026-06-10 14:12:40 +0000

    dbt_arten: taking care of cut off column names of wisgl shapefile export in staging model

--- src/dbt/dbt_arten/models/transformations/staging/stg_imp_wisgl_besonderearten.sql
+++ src/dbt/dbt_arten/models/transformations/staging/stg_imp_wisgl_besonderearten.sql
@@ -6,8 +6,8 @@ with source as (
         {{ adapter.quote("id") }},
         {{ adapter.quote("t_ili_tid") }},
         -- info related to cat_art
-        {{ adapter.quote("name_deutsch") }},
-        {{ adapter.quote("name_lateinisch") }},
+        {{ adapter.quote("name_d") }} as name_deutsch,
+        {{ adapter.quote("name_lat") }} as name_lateinisch,
         {{ adapter.quote("id_art") }}, 
 
         -- info that's mirrored in gl_besonderewaldarten.besonderearten
@@ -19,14 +19,14 @@ with source as (
         -- info that's mirrored in prod_gl_arten.artvorkommen_pt_gl
         {{ adapter.quote("radius") }},
         trim({{ adapter.quote("substrat") }}) substrat,
-        trim({{ adapter.quote("foerdermassnahmen") }}) as foerdermassnahmen,
+        trim({{ adapter.quote("foerdermas") }}) as foerdermassnahmen,
         trim({{ adapter.quote("finder") }}) finder,
         {{ adapter.quote("funddatum") }},
-        {{ adapter.quote("hinzugefuegt_am") }},
+        {{ adapter.quote("hinzugefue") }} as hinzugefuegt_am,
         {{ adapter.quote("kantonsint") }}::boolean as kantonsintern,
         {{ adapter.quote("verwaltung") }}::boolean as verwaltungsintern,
         {{ adapter.quote("status") }}::boolean,
-        trim({{ adapter.quote("bemerkungen") }}) bemerkungen,
+        trim({{ adapter.quote("bemerkunge") }}) bemerkungen,
         {{ adapter.quote("fotos") }},
         {{ adapter.quote("genau") }}::boolean,
         {{ adapter.quote("x_koord") }},
