719f90951dac92eb29803b74b705f8e4a17aea64
Andreas <ayomeer@gmail.com>
2026-06-11 12:16:18 +0000

    syntax error fix

--- src/dbt/dbt_ersatzbiotope/models/transformations/staging/stg_cat_kategorie_ersatzmassnahme.sql
+++ src/dbt/dbt_ersatzbiotope/models/transformations/staging/stg_cat_kategorie_ersatzmassnahme.sql
@@ -11,5 +11,5 @@ with source as (
   )
   select 
     *,
-    {{ var('t_id_starting_values')['kategorie_catalogue'] }} + row_number()-1 over() as future_t_id 
+    {{ var('t_id_starting_values')['kategorie_catalogue'] }} - 1 + row_number() over()  as future_t_id 
   from renamed
