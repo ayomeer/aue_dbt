58e55c901e681148e845a178e3aba516ac51a48d
Andreas <ayomeer@gmail.com>
2026-06-18 10:16:20 +0000

    dbt_biotope: adding ili_mirror models

---
+++ src/dbt/dbt_biotope/models/transformations/export_to_ch_kt_schemas/auengebiete/ili_mirror_kartierungsgrundlage_catalogue.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }} 
+
+SELECT 
+  t_id::bigint,
+  t_basket::bigint,
+  t_ili_tid::character varying(200),
+  acode::character varying(3),
+  adescription::text,
+  adescription_de::text,
+  adescription_fr::text,
+  adescription_rm::text,
+  adescription_it::text,
+  adescription_en::text
+FROM {{ ref('placeholder') }}
---
+++ src/dbt/dbt_biotope/models/transformations/export_to_ch_kt_schemas/auengebiete/ili_mirror_kt_auengebiet_teilobjekt.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }} 
+
+SELECT 
+  t_id::bigint,
+  t_basket::bigint,
+  t_ili_tid::uuid,
+  teilobj_nr::character varying(30),
+  geo_obj::geometry(MultiSurface,2056),
+  kt_auengebiet::bigint
+FROM {{ ref('placeholder') }}
