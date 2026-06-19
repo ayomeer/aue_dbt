40d4c6981d0f5b6fe0bba5a1baeaa64bcde889fb
Andreas <ayomeer@gmail.com>
2026-06-19 10:32:04 +0000

    cleaning up things not needed on this branch

--- src/dbt/dbt_biotope/models/transformations/export_to_ch_kt_schemas/biotope und teilobjekte/biotop.sql
+++ src/dbt/dbt_biotope/models/transformations/export_to_ch_kt_schemas/biotope und teilobjekte/biotop.sql
@@ -5,7 +5,11 @@
 		(SUM(st_area(geometrie)) / 100)::numeric(12,3) as obj_gisflaeche, -- [ha]
     --NULL as au_typ,
     min(herkunft)::varchar(80) as herkunft -- asserted, that they're all the same in tests
-    min
   from {{ ref('stg_biotope_to_sf') }}
 	group by objekt_nummer
+  having 
+    count(distinct objekt_name) = 1 AND
+    count(distinct herkunft) = 1 AND
+    count(distinct kartierungsgrundlage) = 1 AND
+    count(distinct bedeutung) = 1
 	order by objekt_nummer
