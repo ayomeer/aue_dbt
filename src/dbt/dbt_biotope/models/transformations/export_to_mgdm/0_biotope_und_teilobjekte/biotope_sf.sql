b0ba7ee73365f9970b9eb24fd5eb73115bce2092
Andreas <ayomeer@gmail.com>
2026-06-22 14:11:45 +0000

    grouping teilobjekte by all mandatory aggregate attrubutes now as suggested by PS

--- src/dbt/dbt_biotope/models/transformations/export_to_mgdm/0_biotope_und_teilobjekte/biotope_sf.sql
+++ src/dbt/dbt_biotope/models/transformations/export_to_mgdm/0_biotope_und_teilobjekte/biotope_sf.sql
@@ -4,15 +4,15 @@ select
   nextval('{{target.schema}}.t_ili2db_seq'::regclass) as t_id,
   'GL'::varchar as kanton,
   objekt_nummer,
-  min(objekt_name) as aname, -- asserted, that they're all the same in tests
+  objekt_name as aname, -- asserted, that they're all the same in tests
   (SUM(st_area(geometrie)) / 100)::numeric(12,3) as obj_gisflaeche, -- [ha]
   --NULL as au_typ,
-  min(herkunft)::varchar(80) as herkunft, -- asserted, that they're all the same in tests
-  min(kartierungsgrundlage) as kartierungsgrundlage, 
+  herkunft::varchar(80) as herkunft, -- asserted, that they're all the same in tests
+  kartierungsgrundlage, 
   --min(aufnahmedatum)::date as aufnahmedatum,
   --max(intern_letzte_mutation)::date as mutationsdatum,
   --min(intern_mutationsgrund)::text as mutationsgrund,
-  (array_agg(c_bed.adescription_de ORDER BY c_bed.acode ASC))[1]::varchar as bedeutung, -- take highest prio
+  bedeutung::varchar, 
   
   -- added info for splitting and joining biotope into MGDM objects
   sf.biotopart,
@@ -20,10 +20,7 @@ select
 from {{ ref('stg_biotope_to_sf') }} as sf
 left join {{ ref('stg_bedeutung_catalogue') }} as c_bed
   on c_bed.adescription_de = sf.bedeutung
-group by objekt_nummer, biotopart
+group by objekt_nummer, biotopart, bedeutung, kartierungsgrundlage, herkunft, objekt_name
 having 
   count(distinct objekt_name) = 1 AND
-  count(distinct herkunft) = 1 AND
-  count(distinct kartierungsgrundlage) = 1 AND
-  count(distinct bedeutung) = 1 AND
   (SUM(st_area(geometrie)) / 100) > 1.0 -- mgdm constraint: at least 1ha
--- src/dbt/dbt_biotope/tests/test_biotope_to_sf__aggregate_uniqueness.sql
+++ src/dbt/dbt_biotope/tests/test_biotope_to_sf__aggregate_uniqueness.sql
@@ -2,15 +2,10 @@ select
   objekt_nummer,
   array_agg(distinct objekt_name) as agg_distinct_objekt_name,
   array_agg(distinct herkunft) as agg_distinct_herkunft,
-  array_agg(distinct kartierungsgrundlage) as agg_distinct_kartierungsgrundlage,
-  array_agg(distinct bedeutung) as agg_distinct_bedeutung,
   array_agg(distinct intern_mutationsgrund) as agg_distinct_mutationsgrund
 from {{ ref('stg_biotope_to_sf') }}
 group by objekt_nummer
 having 
-  count(distinct objekt_name) > 1 OR
-  count(distinct herkunft) > 1 OR
-  count(distinct kartierungsgrundlage) > 1 OR
-  count(distinct bedeutung) > 1 OR 
-  count(distinct intern_mutationsgrund) > 1
+  count(distinct objekt_name) > 1 
+  -- OR count(distinct intern_mutationsgrund) > 1
   
