d862e853e52aa608c941e365ce13c8c450cf78f8
Andreas <ayomeer@gmail.com>
2026-08-25 09:56:19 +0000

    wmill release script .git remove fix

--- create_wmill_release_git_tag.sh
+++ create_wmill_release_git_tag.sh
@@ -42,7 +42,7 @@ fi
 echo "Project Path: $project_path"
 echo "Release Name: $release_name"
 
-rm -rf -i "${project_path}/dbt_packages/*/.git"
+rm -rf "${project_path}"/dbt_packages/*/.git
 git add -f "${project_path}/dbt_packages"
 git commit -m "wmill release commit"
 git tag $release_name 
