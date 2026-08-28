7e6606c85ce7e468b5d5f1625fa2f6ce877e74b8
Andreas <ayomeer@gmail.com>
2026-08-28 13:57:46 +0000

    wmill release commit

---
+++ src/dbt/dbt_biotope/dbt_packages/ili_utils/macros/ili_utils.sql
@@ -0,0 +1,177 @@
+--- General Utility -----------------------------------------------------------
+
+-- Create t_ili2db_sequence
+-- intended use: first time dbt schema setup
+--  call through run-operations e.g.:
+--  dbt run-operation create_ili_sequence --args '{schema: dbt_quellkataster}'
+{%- macro create_ili_sequence(schema_name) -%}
+  CREATE SEQUENCE IF NOT EXISTS {{schema_name}}.t_ili2db_seq
+  INCREMENT 1
+  START 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  CACHE 1;
+{%- endmacro %}
+
+-- Reset 't_ili2db_seq' in target schema
+{%- macro reset_ili_sequence(schema_name, starting_value=1) -%}
+  {{ log("Restarting " ~ schema_name ~ ".t_ili2db_seq with " ~ starting_value, info=True) }}
+
+  ALTER SEQUENCE {{schema_name}}.t_ili2db_seq RESTART WITH {{starting_value}};
+{%- endmacro %}
+
+-- Updates ili sequence in target schema based on max t_id in calling model.
+-- Intended to use after inserting records with explicit t_ids, rather than letting target schema
+-- assign them.
+{%- macro synch_sequence_with_ili_target(schema_name) -%}
+  WITH agg as (
+    SELECT max(t_id) as max_t_id 
+    FROM {{ this }}
+  )
+  SELECT setval('{{schema_name}}.t_ili2db_seq', agg.max_t_id) 
+  FROM agg
+{%- endmacro -%}
+
+-- Set up roles, such that there are no access issues for the dbt user 
+-- (target.user). Run this before restoring backups, so the roles can be granted.
+{%- macro setup_roles_for_schema(schema_name) -%}
+  {{ log("Creating Read/Write roles for schema " ~ schema_name, info=True) }}
+  {{ log("Creating role " ~ schema_name ~ "_read", info=True) }}
+  {{ log("Creating role " ~ schema_name ~ "_write", info=True) }}
+
+  CREATE ROLE {{ schema_name }}_read;
+  CREATE ROLE {{ schema_name }}_write;
+
+  {{ log("Assigning created roles to " ~ target.user, info=True) }}
+  GRANT {{ schema_name }}_read to {{ target.user }};
+  GRANT {{ schema_name }}_write to {{ target.user }};
+{%- endmacro -%}
+
+{% macro grant_select_on_all_tables(schema_name) -%}
+  {{ log(
+    "Granting USAGE on schema " ~ schema_name 
+    ~ " to " ~ target.user, 
+    info=True
+  )}}
+  {{ log(
+    "Granting ALL on all tables in schema " ~ schema_name 
+    ~ " to " ~ target.user, 
+    info=True
+  )}}
+  GRANT USAGE ON SCHEMA {{schema_name}} TO {{target.user}};
+  GRANT ALL ON ALL TABLES IN SCHEMA {{schema_name}} TO {{target.user}};
+{%- endmacro -%}
+
+
+--- Baskets and Datasets ------------------------------------------------------
+-- Populate basket table based on project configuration:
+-- Add a row (i.e.) a basket for each basket defined in dbt_project.yml
+{%- macro setup_baskets(schema_name, baskets_dict) -%}
+  {% for key, value_dict in baskets_dict.items() %}
+    {{ log(
+        "Writing " ~ key ~ " into " ~ schema_name ~ ".t_ili2db_basket",
+        info=True
+    )}}
+    INSERT INTO {{schema_name}}.t_ili2db_basket(
+      t_id,         -- NOT NULL
+      dataset,
+      topic,        -- NOT NULL
+      t_ili_tid,
+      attachmentkey,-- NOT NULL (but also not used -> write '-')
+      domains
+    )
+    VALUES (
+      {{ value_dict['t_id'] }},
+      {{ value_dict['dataset_t_id'] }},
+      '{{ value_dict['topic'] }}',
+      uuid_generate_v4(),
+      '-',
+      NULL
+    );
+    -- advance t_ili2db_seq to make up for manually set t_id
+    SELECT nextval('{{schema_name}}.t_ili2db_seq'::regclass);
+  {% endfor %}
+{%- endmacro %}
+
+{%- macro setup_datasets(schema_name, datasets_dict) -%}
+  
+  {% for key, value_dict in datasets_dict.items() %}
+    {{ log(
+        "Writing " ~ key ~ " into " ~ schema_name ~ ".t_ili2db_dataset",
+        info=True
+    )}}
+    INSERT INTO {{schema_name}}.t_ili2db_dataset(
+      t_id,
+      datasetname
+    )
+    VALUES (
+      {{ value_dict['t_id'] }},
+      '{{ value_dict['datasetname'] }}'
+    );
+    -- advance t_ili2db_seq to make up for manually set t_id
+    SELECT nextval('{{schema_name}}.t_ili2db_seq'::regclass);
+  {% endfor %}
+{%- endmacro -%}
+
+{%- macro reset_target_schema(schema_name, datasets_dict, baskets_dict) -%}
+  {{ ili_utils.reset_ili_sequence(schema_name) }}
+
+  TRUNCATE TABLE {{schema_name}}.t_ili2db_dataset CASCADE;
+  TRUNCATE TABLE {{schema_name}}.t_ili2db_basket CASCADE;
+
+  {{ ili_utils.setup_datasets(schema_name, datasets_dict) }}
+  {{ ili_utils.setup_baskets(schema_name, baskets_dict) }}
+{%- endmacro -%}
+
+
+
+
+--- Transferring across boundary: dbt -> INTERLIS schema ----------------------
+
+-- Export dbt table to target table
+{%- macro insert_into(schema_name, table_name, truncate_target=false) -%}
+  {{ log(
+      "Inserting into " ~ schema_name ~ "." ~ table_name ~ 
+      " with truncate_target=" ~ truncate_target, 
+      info=true
+    )}}
+
+  {% if truncate_target %}
+    {{ log(
+      "Truncating " ~ schema_name ~ "." ~ table_name,
+      info=true
+    )}}
+    TRUNCATE TABLE {{schema_name}}.{{table_name}};
+  {% endif %}
+
+  INSERT INTO {{schema_name}}.{{table_name}}(
+    -- Get list of column names present in boundary model
+    -- assumption: boundary model column names match target column names 
+    {{ dbt_utils.get_filtered_columns_in_relation(this) | join(',\n  ') }}
+  )
+  SELECT
+    *
+  FROM {{this}};
+
+  {{ ili_utils.synch_sequence_with_ili_target(schema_name) }}
+
+{%- endmacro %}
+
+--- Parsing on dbt Triggers ---------------------------------------------------
+
+{%- macro run_start_parsing(target_ili_schema) -%}
+  SELECT 1;
+
+  {{ log(
+      "Running ili_utils.run_start_parsing() macro \n"
+      ~ "enable_transfer: " ~ var('enable_transfer', false),
+      info=True
+  )}}
+
+  {{ ili_utils.reset_ili_sequence(target.schema) }}
+
+  {% if var('enable_transfer', false) %}
+    {{ ili_utils.reset_target_schema(target_ili_schema) }}
+  {% endif %}
+
+{%- endmacro %}
