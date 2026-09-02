373b420c8df08fd33997456177ef2deb506b52be
Andreas Kuhn (VM) <andreas.kuhn@gl.ch>
2026-09-02 14:50:31 +0000

    allow multiple objects being created corresponding to different BIO_TYP assignments

--- src/dbt/dbt_biotope/tests/test_biotope_to_sf__aggregate_uniqueness.sql
+++ src/dbt/dbt_biotope/tests/test_biotope_to_sf__aggregate_uniqueness.sql
@@ -1,8 +1,7 @@
 select 
   objekt_nummer,
   array_agg(distinct objekt_name) as agg_distinct_objekt_name,
-  array_agg(distinct herkunft) as agg_distinct_herkunft,
-  array_agg(distinct bio_typ_derived) as agg_bio_typ
+  array_agg(distinct herkunft) as agg_distinct_herkunft
 from {{ ref('stg_biotope_to_sf') }}
 group by 
   -- attributes that are supposed to lead to distinct groupings  
@@ -10,8 +9,9 @@ group by
   biotopart, 
   bedeutung,
   kartierungsgrundlage,
-  herkunft
+  herkunft,
+  bio_typ_derived
 having 
   -- attributes that are assumed to NOT lead to differ within groupings
-  count(distinct objekt_name) > 1 OR
-  count(distinct bio_typ_derived) > 1
+  count(distinct objekt_name) > 1 
+
